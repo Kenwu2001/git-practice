dbe862d4cca2dd05b603147974b8ffc098fa4c19
s109000114 <imeukg2001@gmail.com>
2024-09-30 22:03:28 +0800

    feat: callback.js Promise version

--- week-03/callback.js
+++ week-03/callback.js
@@ -6,6 +6,8 @@ function doJob(job, time, cb) {
     }, time);
 }
 
+const { resolve } = require("path");
+
 // 刷牙 1sec -> 吃早餐 3 sec -> 寫功課 1sec -> 吃午餐 2sec
 let now = new Date();
 console.log(`開始工作 at ${now.toISOString()}`);
@@ -24,3 +26,34 @@ doJob('刷牙', 1000, function (data) {
         });
     });
 });
+
+// TODO: Promise version
+// function doJobPromise(job, time) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             let now = new Date();
+//             resolve(`完成工作 ${job} at ${now.toISOString()}`);
+//         }, time);
+//     });
+// }
+
+// let now = new Date();
+// console.log(`開始工作 at ${now.toISOString()}`);
+
+// doJobPromise('刷牙', 1000)
+//     .then((data) => {
+//         console.log(data);
+//         return doJobPromise('吃早餐', 3000);
+//     })
+//     .then((data) => {
+//         console.log(data);
+//         return doJobPromise('寫功課', 2000);
+//     })
+//     .then((data) => {
+//         console.log(data);
+//         return doJobPromise('吃午餐', 2000);
+//     })
+//     .then((data) => {
+//         console.log(data);
+//     });
+
